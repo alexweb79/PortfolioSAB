2e181953a685e133dab2392d0c018eaf0f900203
Aleksandr Smirnov <alexweb1979@gmail.com>
2017-09-27 10:18:27 +0300

    fix fadein main menu

--- src/scripts/common/hamburger.js
+++ src/scripts/common/hamburger.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var hamb = document.querySelector(".hamburger");
     var hidden = document.querySelector(".hidden");
+    var fadeIn = document.querySelector(".hidden");
     hamb.addEventListener("click", function() {
         hamb.classList.add("is-active");
         hidden.classList.add("visible");
+        fadeIn.classList.add("fadeIn");
 
         hamb.addEventListener("click", function() {
             hamb.classList.toggle("is-active");
             hidden.classList.toggle("visible");
+            fadeIn.classList.toggle("fadeIn");
         });
     });
 });
