9b60aae4434bef978d5447350ebd42e7cfbdf6bf
Aleksandr Smirnov <alexweb1979@gmail.com>
2017-09-27 22:44:34 +0300

    gix scroll button and title blog

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,11 @@
+'use strict';
 //const slider = require('./common/slider');
 //const $ = require('jquery'); // если будет нужен
 const hamburger = require('./common/hamburger');
 const flip = require('./common/flip');
+const arrowDown = require('./common/arrowDown');
 
 //slider(); // инициализируем слайдер
 hamburger();
 flip();
+arrowDown();
