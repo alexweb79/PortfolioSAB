d0cc6ef08f989b9dc98c30732c53d2751678fc27
Aleksandr Smirnov <alexweb1979@gmail.com>
2017-10-07 16:37:16 +0300

    add JS slader

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,41 +1,38 @@
 'use strict';
+
 const $ = require('jquery'); // если будет нужен
-// const preloader = require('./common/preloader');
-// const hamburger = require('./common/hamburger');
-// const flip = require('./common/flip');
-// const arrowDown = require('./common/arrowDown');
-// const pageScrollMenu = require('./common/pageScrollMenu');
-// const validationForm = require('./common/validationForm');
-// const validForm = require('./common/validForm');
 import preloader from './common/preloader';
 import hamburger from './common/hamburger';
 import flip from './common/flip';
 import arrowDown from './common/arrowDown';
 import pageScrollMenu from './common/pageScrollMenu';
+import slider from './common/slider';
 import validationForm from './common/validationForm';
-import validForm from './common/validForm';
 
-// preloader();
-// hamburger();
-// flip();
-// arrowDown();
-// pageScrollMenu();
-// validationForm();
-// validForm();
-$(() => {
-    preloader.init({
-        waiting: true
-    });
-    // preloader.init();
-    hamburger.init();
-    flip.init();
-    arrowDown.init();
-    pageScrollMenu.init();
-    validForm.init($('.about-me-form'));
-    comboSlider.init($('.combo-slider'));
-    // validationForm.init({
-    //     $el: $('.contact-form'),
-    //     $submitButton: $('.contact-form-submit'),
-    //     $resetButton: $('.contact-form-reset')
-    // });
+document.addEventListener('DOMContentLoaded', function() {
+    slider();
+    preloader();
+    hamburger();
+    flip();
+    arrowDown();
+    pageScrollMenu();
+    validationForm();
 });
+
+// $(() => {
+//     preloader.init({
+//         waiting: true
+//     });
+//     preloader.init();
+//     hamburger.init();
+//     flip.init();
+//     arrowDown.init();
+//     pageScrollMenu.init();
+//     slider.init($('.slider-1'));
+//     validForm.init($('.submit'));
+//     validationForm.init({
+//         $el: $('.contact-form'),
+//         $submitButton: $('.contact-form-submit'),
+//         $resetButton: $('.contact-form-reset')
+//     });
+// });
